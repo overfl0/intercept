b83aef02990e361978cd83b9feb737ec7d2c0063
Lukasz Taczuk <overfl0@users.noreply.github.com>
2017-07-04 12:49:24 +0200

    Create travis_install_cross_compiler.sh

---
+++ tools/travis_install_cross_compiler.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export PATH=$PATH:/opt/mxe/usr/bin
+
+if [ -f /opt/mxe/usr/bin/i686-w64-mingw32.static.posix-cmake ]
+then
+    echo "Cross-compiler found, continuing..."
+    exit 0
+fi
+
+echo "###################################################################################################";
+echo "No cross-compiler found in the cached directory. Building it using MXE. This will take a long time!";
+echo "###################################################################################################";
+
+mkdir -p /opt;
+rm -rf /opt/mxe;
+git clone https://github.com/overfl0/mxe /opt/mxe;
+pushd /opt/mxe;
+echo "Building...";
+make -j 3 MXE_PLUGIN_DIRS=plugins/gcc7 MXE_TARGETS='x86_64-w64-mingw32.static.posix i686-w64-mingw32.static.posix' gcc;
+
+ls -l /opt/mxe/usr/bin/;
+popd;
