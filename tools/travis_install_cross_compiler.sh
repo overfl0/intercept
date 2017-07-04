8985c424677c1bda49406b5b4626446446976560
Lukasz Taczuk <overfl0@users.noreply.github.com>
2017-07-05 00:52:33 +0200

    Doc

--- tools/travis_install_cross_compiler.sh
+++ tools/travis_install_cross_compiler.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# This is a script that builds a mingw cross compiler for compiling windows
+# binaries on linux.
+# It uses mxe to compile the latest mingw with gcc7.
+# It also assumes that /opt is a directory that is cached by Travis so it
+# checks if the cross compiler has been built previously and does nothing if
+# it is already there.
+# Note: You have to use the x86_64 compiler to generate 64-bit binaries and
+# i686 for 32-bit binaries, otherwise it won't work.
+
 CROSS_COMPILER_PATH=/opt/mxe/usr/bin
 export PATH="$PATH:${CROSS_COMPILER_PATH}"
 
